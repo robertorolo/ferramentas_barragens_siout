5127770d0bce0eca94b956b6f9841c54031ab169
Roberto Rolo <robertorolo@gmail.com>
2020-11-23 09:47:29 -0300

    Update pumpcheck.py

--- pumpcheck.py
+++ pumpcheck.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
 import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = 8
 
 def quit_me():
     print('quit')
